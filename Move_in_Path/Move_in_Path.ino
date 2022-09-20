e6c509d968aa216619474f4e2cadb61b2652af00
Aya-Hazem <aya.hazem6103@gmail.com>
2022-09-20 19:46:20 +0200

    1.03

--- Move_in_Path/Move_in_Path.ino
+++ Move_in_Path/Move_in_Path.ino
@@ -1,300 +1,402 @@
 #include <util/atomic.h>
 
-#define trig0 A1
-#define trig1 8
-#define trig2 A3
-#define trig3 A5
-
-#define echo0 A0
-#define echo1 9
-#define echo2 A2
-#define echo3 A4
+#define IRL A7
+#define IRR A6
 
 #define encAR 2
 #define encBR 5
-#define inAR 10
-#define inBR 11
+#define inAR 11
+#define inBR 10
 #define PWMR 9
 
 #define encAL 3
 #define encBL 4
-#define inAL 8
-#define inBL 7
+#define inAL 7
+#define inBL 8
 #define PWML 6
 
-volatile int posR = 0 , posL = 0 ;
+#define trigR 12
+#define trigL A3
+#define trigF A1
+
+#define echoR A0
+#define echoL A4
+#define echoF A2
+
+volatile int posR = 0, posL = 0;
 long prevT = 0;
-float prevER = 0 ,prevEL = 0  ;
-float integralER = 0 ,integralEL = 0  ;
-
+float prevER = 0, prevEL = 0;
+float integralER = 0, integralEL = 0;
 int target;
+int cellLength = 30;
+int dirs[] = { 1, 2, 4, 8 };
+int currDir = 1;
+int nextDir;
+int X, Y;
 
-const int cellLength = 30; 
+
+
+struct coord {
+  int x;
+  int y;
+};
+
+coord start = { 0, 0 };
+coord end = { 2, 2 };
+coord curr = start;
+coord minCoord;
+int minDist = 100;
+int nextDist;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(encAR,INPUT);
-  pinMode(encBR,INPUT);
-  pinMode(inAR,OUTPUT);
-  pinMode(inBR,OUTPUT);
-  pinMode(PWMR,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(encAR),readEncoderR,RISING);
+  pinMode(encAR, INPUT);
+  pinMode(encBR, INPUT);
+  pinMode(inAR, OUTPUT);
+  pinMode(inBR, OUTPUT);
+  pinMode(PWMR, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(encAR), readEncoderR, RISING);
 
-  pinMode(encAL,INPUT);
-  pinMode(encBL,INPUT);
-  pinMode(inAL,OUTPUT);
-  pinMode(inBL,OUTPUT);
-  pinMode(PWML,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(encAL),readEncoderL,RISING);
+  pinMode(encAL, INPUT);
+  pinMode(encBL, INPUT);
+  pinMode(inAL, OUTPUT);
+  pinMode(inBL, OUTPUT);
+  pinMode(PWML, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(encAL), readEncoderL, RISING);
 
-  pinMode(trig0,OUTPUT);
-  pinMode(echo0,INPUT);
+  pinMode(trigR, OUTPUT);
+  pinMode(echoR, INPUT);
+  pinMode(trigL, OUTPUT);
+  pinMode(echoL, INPUT);
+  pinMode(trigF, OUTPUT);
+  pinMode(echoF, INPUT);
 
-  pinMode(trig1,OUTPUT);
-  pinMode(echo1,INPUT);
-
-  pinMode(trig2,OUTPUT);
-  pinMode(echo2,INPUT);
-
-  pinMode(trig3,OUTPUT);
-  pinMode(echo3,INPUT);
+  pinMode(IRR, INPUT);
+  pinMode(IRL, INPUT);
 }
 
-void loop() { 
-/* N >> N >> N >> N >> E >> E >> N >> N >> w */
-  byte path[] = {1,1,1,1,4,4,1,1,8};
-  byte currHeading = path[0];
-  for(int i = 0 ; i < sizeof(path) ; i++){
-    moveTo(path[i],currHeading,200);
-    
-    currHeading = path[i];
-    }
-  /*
-    N to E
-    turn 90  *for x milie soconds* or *until both ultrasonics reads walls*
-    continue forward
-  */
+void loop() {
+  if (readUltrasonicF() > 20) {
+    moveTo(currDir, currDir, 100);
+  } else if (readUltrasonicL() > 20) {
+    nextDir = currDir >> 1;
+    if (nextDir <= 0) nextDir = 8;
+    else if (nextDir >= 16) nextDir = 1;
+    moveTo(nextDir, currDir, 100);
+  } else if (readUltrasonicR() > 20) {
+    nextDir = currDir << 1;
+    if (nextDir <= 0) nextDir = 8;
+    else if (nextDir >= 16) nextDir = 1;
+    moveTo(nextDir, currDir, 100);
+  }
+  delay(100);
 }
-void moveDist(int dist , int Speed){
+
+void moveDist(int dist, int Speed) {
   int ppr = 385;
-  float tireCirc = 2 * 3.25 * 3.14;
-  target = (float(dist)/tireCirc ) * float(ppr);
-  float kpL = 0.45 , kiL = 0.1, kdL = 0;
-  float kpR = 0.45, kiR = 0.1, kdR = 0;
-  long currT = micros();
-  float deltaT = float(currT - prevT)/1.0e6;
-  int currPosR ,currPosL;
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+  float tireCirc = 2 * 3.5 * 3.14;
+  int currPosR, currPosL;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
     currPosR = posR;
     currPosL = posL;
-    }
-
-  int eL = currPosL  -  target, eR = target + currPosR  ;
-  int dedER = (eR- prevER) / deltaT , dedEL = (eL - prevEL) / deltaT ;
-  integralER = integralER + eR*deltaT;
-  integralEL = integralEL + eL*deltaT;
-  
-  float uR = kpR*eR + kiR * integralER  + kdR * dedER;
-  float uL = kpL*eL + kiL * integralEL  + kdL * dedEL;
-  while((uL > 50 || uL < - 50) || (uR > 50 || uR < - 50)){
-    int pwmR = (fabs(uR)) * Speed , pwmL = (fabs(uL)) * Speed;
-      if(pwmR > Speed){
-        pwmR = Speed;
-        }
-      if(pwmL > Speed){
-        pwmL = Speed;
-        }
-      int dirR = 1 , dirL = 1;
-      if(uR < 0){
-         dirR = -1;
+  }
+  int targetR = -((float(dist) / tireCirc) * float(ppr)) + currPosR;
+  int targetL = (float(dist) / tireCirc) * float(ppr) + currPosL;
+  int eL = targetL - currPosL, eR = currPosR - targetR;
+  int dirR = 1, dirL = 1;
+  if (eR < 0) dirR = -1;
+  if (eL < 0) dirL = -1;
+  int mn = 0.02 * abs(eR);
+  while (abs(eR) > mn) {
+    // if (readUltrasonicF() < 10) {
+    //   setMotor(0, 0, inAR, inBR, PWMR);
+    //   setMotor(0, 0, inAL, inBL, PWML);
+    //   break;
+    // }
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      // int SPEED = (int)0.8 * Speed;
+      while (analogRead(IRL) < 100) {
+        setMotor(-1, 80, inAR, inBR, PWMR);
+        setMotor(1, 80, inAL, inBL, PWML);
       }
-       if(uL < 0){
-         dirL = -1;
+      while (analogRead(IRR) < 100) {
+        setMotor(1, 80, inAR, inBR, PWMR);
+        setMotor(-1, 80, inAL, inBL, PWML);
       }
-      
-      setMotor(dirR,pwmR,inAR,inBR,PWMR);
-      setMotor(dirL,pwmL,inAL,inBL,PWML);
-      
-      prevER = eR; prevEL = eL;
-      prevT = currT ;
-      currT = micros();
-      deltaT = float(currT - prevT)/1.0e6;
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-        currPosR = posR;
-        currPosL = posL;
-        }
-    
-      eL =   target - currPosL, eR =  target + currPosR   ;
-      dedER = (eR- prevER) / deltaT , dedEL = (eL - prevEL) / deltaT ;
-      integralER = integralER + eR*deltaT;
-      integralEL = integralEL + eL*deltaT;
-      
-      uR = kpR*eR + kiR * integralER  + kdR * dedER;
-      uL = kpL*eL + kiL * integralEL  + kdL * dedEL;
-       }
-      setMotor(0,0,inAR,inBR,PWMR);
-      setMotor(0,0,inAL,inBL,PWML); 
-  }
-void setMotor(int dir , int pwmValue , int in1 , int in2 , int pwm ){
-  analogWrite(pwm , pwmValue);
-  if(dir == 1){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
     }
-  else if(dir == -1){
-    digitalWrite(in2,HIGH);
-    digitalWrite(in1,LOW);
+    int pwmR = float(0.45 * abs(eR)) * Speed, pwmL;
+    if (pwmR > Speed) {
+      pwmR = Speed;
     }
-  else{
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    }
-  }
-void readEncoderR(){
-  if(digitalRead(encBR)) posR++;
-  else posR--;
-  }
-
-void readEncoderL(){
-  if(digitalRead(encBL)) posL++;
-  else posL--;
-  }
-
-void turnAround(int degree , int Speed){
-  int ppr = 385;
-  int targetL = degree * float(ppr) , targetR = degree * float(ppr);
-  float kpL = 0.45 , kiL = 0.1, kdL = 0;
-  float kpR = 0.45, kiR = 0.1, kdR = 0;
-  long currT = micros();
-  float deltaT = float(currT - prevT)/1.0e6;
-  int currPosR ,currPosL;
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    currPosR = posR;
-    currPosL = posL;
-    }
-
-  int eL = currPosL  -  targetL, eR = targetR - currPosR  ;
-  int dedER = (eR- prevER) / deltaT , dedEL = (eL - prevEL) / deltaT ;
-  integralER = integralER + eR*deltaT;
-  integralEL = integralEL + eL*deltaT;
-  
-  float uR = kpR*eR + kiR * integralER  + kdR * dedER;
-  float uL = kpL*eL + kiL * integralEL  + kdL * dedEL;
-  while((uL > 50 || uL < - 50) || (uR > 50 || uR < - 50)){
-    int pwmR = (fabs(uR)) * Speed , pwmL = (fabs(uL)) * Speed;
-      if(pwmR > Speed){
-        pwmR = Speed;
-        }
-      if(pwmL > Speed){
-        pwmL = Speed;
-        }
-      int dirR = 1 , dirL = 1;
-      if(uR < 0){
-         dirR = -1;
-      }
-       if(uL < 0){
-         dirL = -1;
-      }
-      
-      setMotor(dirR,pwmR,inAR,inBR,PWMR);
-      setMotor(dirL,pwmL,inAL,inBL,PWML);
-      
-      prevER = eR; prevEL = eL;
-      prevT = currT ;
-      currT = micros();
-      deltaT = float(currT - prevT)/1.0e6;
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-        currPosR = posR;
-        currPosL = posL;
-        }
-    
-      eL = currPosL  -  targetL, eR = targetR - currPosR;
-      dedER = (eR- prevER) / deltaT , dedEL = (eL - prevEL) / deltaT ;
-      integralER = integralER + eR*deltaT;
-      integralEL = integralEL + eL*deltaT;
-      
-      uR = kpR*eR + kiR * integralER  + kdR * dedER;
-      uL = kpL*eL + kiL * integralEL  + kdL * dedEL;
-       }
-      setMotor(0,0,inAR,inBR,PWMR);
-      setMotor(0,0,inAL,inBL,PWML); 
-  }
- 
-void moveTo(int heading,int currHeading , int Speed){
-  switch (heading) {
-    case 1 :
-        switch (currHeading) {
-          case 1 :
-            moveDist(cellLength,Speed);
-            break;
-          case 2 :
-            turnAround(-180,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 4 :
-            turnAround(-90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 8 :
-            turnAround(90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-        break;}
-    case 2 : /*N >> S*/
-    switch (currHeading) {
-          case 1 :
-            turnAround(180,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 2 :
-            moveDist(cellLength,Speed);
-            break;
-          case 4 :
-            turnAround(90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 8 :
-            turnAround(-90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-        break;
-        }
-    case 4 :
-        switch (currHeading) {
-          case 1 :
-            turnAround(-90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 2 :
-            turnAround(90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 4 :
-            moveDist(cellLength,Speed);
-            break;
-          case 8 :
-            turnAround(180,Speed);
-            moveDist(cellLength,Speed);
-            break;
-         break;}
-    case 8 :
-        switch (currHeading) {
-          case 1 :
-            turnAround(90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 2 :
-            turnAround(-90,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 4 :
-            turnAround(180,Speed);
-            moveDist(cellLength,Speed);
-            break;
-          case 8 :
-            moveDist(cellLength,Speed);
-            break;
+    dirR = 1, dirL = 1;
+    if (eR < 0) {
       break;
     }
+    if (eL < 0) {
+      break;
+    }
+    if (abs(eL) < abs(eR)) {
+      pwmL = pwmR - 0.15 * pwmR;
+      pwmR = pwmR + 0.15 * pwmR;
+    } else if (abs(eL) > abs(eR)) {
+      pwmL = pwmR + 0.15 * pwmR;
+      pwmR = pwmR - 0.15 * pwmR;
+    }
+    setMotor(dirR, pwmR, inAR, inBR, PWMR);
+    setMotor(dirL, pwmL, inAL, inBL, PWML);
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      currPosR = posR;
+      currPosL = posL;
+    }
+    eL = targetL - currPosL, eR = currPosR - targetR;
+  }
+  setMotor(0, 0, inAR, inBR, PWMR);
+  setMotor(0, 0, inAL, inBL, PWML);
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    posR = 0;
+    posL = 0;
+  }
+}
+
+bool isValid(int x, int y) {
+  return ((x > 4) || (x < 0) || (y > 4) || (y < 0)) ? false : true;
+}
+void setMotor(int dir, int pwmValue, int in1, int in2, int pwm) {
+  analogWrite(pwm, pwmValue);
+  if (dir == 1) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  } else if (dir == -1) {
+    digitalWrite(in2, HIGH);
+    digitalWrite(in1, LOW);
+  } else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  }
+}
+void readEncoderR() {
+  if (digitalRead(encBR)) posR++;
+  else posR--;
+}
+
+void readEncoderL() {
+  if (digitalRead(encBL)) posL++;
+  else posL--;
+}
+
+
+void turnAround(int degree, int Speed) {
+  int fullTurn = 920;
+  int target;
+  switch (degree) {
+    case 90:
+      target = 0.25 * fullTurn;
+      break;
+    case -90:
+      target = -0.25 * fullTurn;
+      break;
+    case 180:
+      target = 0.52 * fullTurn;
+      break;
+    case -180:
+      target = -0.52 * fullTurn;
+      break;
+    case 360:
+      target = fullTurn;
+      break;
+  }
+
+  int currPosR, currPosL;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    currPosR = posR;
+    currPosL = posL;
+  }
+  int targetR = target + currPosR, targetL = target + currPosL;
+  int eL = targetL - currPosL, eR = currPosR - targetR;
+  int dirR = 1, dirL = 1;
+  if (eR < 0) {
+    dirR = -1;
+  }
+  if (eL < 0) {
+    dirL = -1;
+  }
+  int mn = 0.05 * abs(eR);
+
+  while (abs(eR) > mn) {
+    int pwmR = float(0.45 * abs(eR)) * Speed, pwmL;
+    if (pwmR > Speed) {
+      pwmR = Speed;
+    }
+    // if (eR < 0) {
+    //   break;
+    // }
+    // if (eL < 0) {
+    //   break;
+    // }
+    if (abs(eL) < abs(eR)) {
+      pwmL = pwmR - 0.15 * pwmR;
+      pwmR = pwmR + 0.15 * pwmR;
+    } else if (abs(eL) > abs(eR)) {
+      pwmL = pwmR + 0.15 * pwmR;
+      pwmR = pwmR - 0.15 * pwmR;
+    }
+    setMotor(dirR, pwmR, inAR, inBR, PWMR);
+    setMotor(dirL, pwmL, inAL, inBL, PWML);
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      currPosR = posR;
+      currPosL = posL;
+    }
+    eL = targetL - currPosL, eR = currPosR - targetR;
+  }
+  setMotor(0, 0, inAR, inBR, PWMR);
+  setMotor(0, 0, inAL, inBL, PWML);
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    posR = 0;
+    posL = 0;
+  }
+}
+
+
+int readUltrasonicR() {
+  digitalWrite(trigR, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigR, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigR, LOW);
+  int distant = pulseIn(echoR, HIGH);
+  int disC = distant * 0.5 * 0.034;
+  return disC;
+}
+
+int readUltrasonicL() {
+  digitalWrite(trigL, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigL, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigL, LOW);
+  int distant = pulseIn(echoL, HIGH);
+  int disC = distant * 0.5 * 0.034;
+  return disC;
+}
+
+int readUltrasonicF() {
+  digitalWrite(trigF, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigF, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigF, LOW);
+  int distant = pulseIn(echoF, HIGH);
+  int disC = distant * 0.5 * 0.034;
+  return disC;
+}
+int updateWalls() {
+  int newWalls = 15;
+  int wallsToAdd = 0;
+  int distance;
+  distance = readUltrasonicF();
+  if (distance < 20) {
+    if (newWalls & 1) {
+      wallsToAdd += 1;
+    }
+  }
+  distance = readUltrasonicL();
+  if (distance < 20) {
+    if (newWalls & 8) {
+      wallsToAdd += 8;
+    }
+  }
+  distance = readUltrasonicR();
+  if (distance < 20) {
+    if (newWalls & 2) {
+      wallsToAdd += 2;
+    }
+  }
+  newWalls -= wallsToAdd;
+  return newWalls;
+}
+
+int calcDist(int posX, int posY) {
+  int distance = 0;
+  distance = abs(2 - posX) + abs(2 - posY);
+  return distance;
+}
+void moveTo(int heading, int currHeading, int Speed) {
+  switch (heading) {
+    case 1:
+      switch (currHeading) {
+        case 1:
+          moveDist(cellLength, Speed);
+          break;
+        case 2:
+          turnAround(-90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 4:
+          turnAround(-180, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 8:
+          turnAround(90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+      }
+      break;
+    case 2: /*N >> S*/
+      switch (currHeading) {
+        case 1:
+          turnAround(90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 2:
+          moveDist(cellLength, Speed);
+          break;
+        case 4:
+          turnAround(-90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 8:
+          turnAround(180, Speed);
+          moveDist(cellLength, Speed);
+          break;
+      }
+      break;
+    case 4:
+      switch (currHeading) {
+        case 1:
+          turnAround(180, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 2:
+          turnAround(90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 4:
+          moveDist(cellLength, Speed);
+          break;
+        case 8:
+          turnAround(-90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+      }
+      break;
+    case 8:
+      switch (currHeading) {
+        case 1:
+          turnAround(-90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 2:
+          turnAround(180, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 4:
+          turnAround(90, Speed);
+          moveDist(cellLength, Speed);
+          break;
+        case 8:
+          moveDist(cellLength, Speed);
+          break;
+      }
+      break;
   }
 }
